eaf10f94506ad644ca87ef6218c600a30edd44cf
jfilipek1 <47739228+jfilipek1@users.noreply.github.com>
2020-04-30 01:14:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var instasearchApp = new Vue({
 	
 	mounted() {
 		axios
-			.get('https://picsum.photos/v2/list?page=2&limit=10')
+			.get('https://jfilipek1.github.io/test/clubs.json')
 			.then(response => {
 				this.photoFeed = response.data;
 			})
